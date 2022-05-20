6b9136c597ee91d3fb121064bfe6d286e8afa37a
Asthriona <im@asthriona.com>
2022-05-20 20:02:17 +0200

    chore: linting

--- src/views/blog/postView.vue
+++ src/views/blog/postView.vue
@@ -1,141 +1,201 @@
 <template>
-    <div class="postSwow">
-        <div class="thumbnail" v-bind:style="{ 'background-image': 'url(' + post.image + ')' }"></div>
-        <v-container>
-            <v-row>
-                <v-col cols="12">
-                    <div class="headline text-center">
-                        <span>{{ post.title }}</span>
-                    </div>
-                    <div class="subtitle text-center">
-                        <i class="muted">posted on {{ new Date() | moment("MM Do YYYY") }}</i> <br>
-                        <i v-if="post.updatedAt">last Update: {{ post.update | moment("dddd, MM Do YYYY") }}</i>
-                    </div>
-                </v-col>
-                <v-col cols="12" class="post-body">
-                    <div class="post-content">
-                        <div class="post-content-text">
-                            <span class="content" v-html="post.sanitizedHtml"></span>
-                        </div>
-                    </div>
-                </v-col>
-                <v-spacer></v-spacer>
-                <hr />
-                <v-col cols="12">
-                    <div class="author">
-                        <div class="headline">
-                            <p>About the author:</p>
-                        </div>
-                        <div class="author-img">
-                            <v-avatar
-                            size="150">
-                                <v-img :src="author.avatar"></v-img>
-                            </v-avatar>
-                            <span class="ml-4 display-2">{{ author.displayName }}#{{ author.discriminator }} <v-icon size="50" class="mb-2">{{ author.selectedBadge }}</v-icon></span> <br>
-                            <span class="ml-12 subtitle">{{ author.description }}</span>
-                        </div>
-                    </div>
-                </v-col>
-            </v-row>
-        </v-container>
-        <div class="comments">
-            <v-container>
-                <v-row>
-                    <v-col cols="12">
-                        <div class="headline">
-                            <p>post new comments</p>
-                            <v-form>
-                                <v-textarea
-                                v-model="newComment"
-                                label="Comment"
-                                :rules="commentRules"
-                                ></v-textarea>
-                                <v-btn
-                                color="primary"
-                                type="submit"
-                                @click="submitComment"
-                                >
-                                Post
-                                </v-btn>
-                            </v-form>
-                        </div>
-                    </v-col>
-                    <v-col cols="12">
-                        <div class="headline">
-                            <p>Comments</p>
-                        </div>
-                    </v-col>
-                    <div class="nocomment" v-if="!comments">
-                    <p>be the first to comment to this post!</p>
-                    </div>
-                <div class="comment" v-else>
-                    <v-col v-for="comment in comments" :key="comment.id">
-                    <v-avatar>
-                        <v-img :src="comment.author.avatar"></v-img>
-                    </v-avatar>
-                        <span class="headline ml-2">{{ comment.author.displayName }}#{{ comment.author.discriminator }} <v-icon>{{ comment.author.selectedBadge }}</v-icon></span>
-                    <div class="comment-body ml-4 mt-4">
-                        <div class="comment-body-text ml-4">
-                            <span class="content" v-html="comment.content"></span>
-                        </div>
-                        </div>
-                    </v-col>
+  <div class="postSwow">
+    <div
+      class="thumbnail"
+      v-bind:style="{ 'background-image': 'url(' + post.image + ')' }"
+    ></div>
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <div class="headline text-center">
+            <span>{{ post.title }}</span>
+          </div>
+          <div class="subtitle text-center">
+            <i class="muted"
+              >posted on {{ new Date() | moment("MM Do YYYY") }}</i
+            >
+            <br />
+            <i v-if="post.updatedAt"
+              >last Update: {{ post.update | moment("dddd, MM Do YYYY") }}</i
+            >
+          </div>
+        </v-col>
+        <v-col cols="12" class="post-body">
+          <div class="post-content">
+            <div class="post-content-text">
+              <span class="content" v-html="post.sanitizedHtml"></span>
+            </div>
+          </div>
+        </v-col>
+        <v-spacer></v-spacer>
+        <hr />
+        <v-col cols="12">
+          <div class="author">
+            <div class="headline">
+              <p>About the author:</p>
+            </div>
+            <div class="author-img">
+              <v-avatar size="150">
+                <v-img :src="author.avatar"></v-img>
+              </v-avatar>
+              <span class="ml-4 display-2"
+                >{{ author.displayName }}#{{ author.discriminator }}
+                <v-icon size="50" class="mb-2">{{
+                  author.selectedBadge
+                }}</v-icon></span
+              >
+              <br />
+              <span class="ml-12 subtitle">{{ author.description }}</span>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+    <div class="comments">
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <div class="headline">
+              <p>post new comments</p>
+              <!-- if user is banned. -->
+              <v-alert type="error" class="text-center" v-if="user.isBanned">
+                <span class="headline">
+                  <v-icon>mdi-alert-circle</v-icon>
+                  <span>
+                    Banned account are unable to post comments. <br />
+                    Reason: {{ user.banReason }} <br />
+                    Expire: {{ user.banExpired || "Never" }}
+                  </span>
+                </span>
+              </v-alert>
+              <v-form v-else-if="newCommentSent == false">
+                <v-textarea
+                  v-model="newComment.content"
+                  label="Comment"
+                  :rules="commentRules"
+                ></v-textarea>
+                <v-btn color="primary" type="submit" @click="submitComment">
+                  Post
+                </v-btn>
+              </v-form>
+              <v-alert type="success" class="text-center" v-else>
+                <span class="headline">
+                  Thanks for commenting! (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
+                </span>
+              </v-alert>
+            </div>
+          </v-col>
+          <v-col cols="12">
+            <div class="headline">
+              <p>Comments</p>
+            </div>
+          </v-col>
+          <div class="coms">
+            <div class="nocomment headline" v-if="comments.length == 0">
+              <p>be the first to comment to this post!</p>
+            </div>
+            <div class="comment" v-else>
+              <v-col v-for="comment in comments" :key="comment.id">
+                <v-avatar>
+                  <v-img :src="comment.author.avatar"></v-img>
+                </v-avatar>
+                <span class="headline ml-2"
+                  >{{ comment.author.displayName }}#{{
+                    comment.author.discriminator
+                  }}
+                  <v-icon>{{ comment.author.selectedBadge }}</v-icon></span
+                >
+                <div class="comment-body ml-4 mt-4">
+                  <div class="comment-body-text ml-4">
+                    <span class="content" v-html="comment.content"></span>
+                  </div>
                 </div>
-                </v-row>
-            </v-container>
-        </div>
+              </v-col>
+            </div>
+          </div>
+        </v-row>
+      </v-container>
     </div>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-    name: "PostView",
-    data() {
-        return {
-            post: {},
-            author: {},
-            comments: [],
-            newComment: "",
-            commentRules: [
-                v => !!v || "Comment is required"
-            ]
-            }
-        },
-    mounted() {
-        axios.get(`${process.env.VUE_APP_URI}blog/posts/asthriona.space/${this.$route.params.slug}`).then((res) => {
-            this.post = res.data.post;
-            this.author = res.data.author;
-        });
-        axios.get(`${process.env.VUE_APP_URI}blog/comments/asthriona.space/${this.$route.params.slug}`)
-        .then((res) => {
-            this.comments = res.data.comments;
-        });
+  name: "PostView",
+  props: ["user"],
+  data() {
+    return {
+      post: {},
+      author: {},
+      comments: [],
+      newComment: {},
+      newCommentSent: false,
+      commentRules: [(v) => !!v || "Comment is required"],
+      loading: {
+        post: true,
+        comment: true,
+      },
+    };
+  },
+  mounted() {
+    // request to whoamI
+    axios.get(`${process.env.API_URL}/whoami`);
+    axios
+      .get(
+        `${process.env.VUE_APP_URI}blog/posts/asthriona.space/${this.$route.params.slug}`
+      )
+      .then((res) => {
+        this.post = res.data.post;
+        this.author = res.data.author;
+      });
+    this.loading.comment = true;
+    axios
+      .get(
+        `${process.env.VUE_APP_URI}blog/comments/asthriona.space/${this.$route.params.slug}`
+      )
+      .then((res) => {
+        this.comments = res.data.comments;
+        this.loading.comment = false;
+      });
+  },
+  methods: {
+    submitComment(e) {
+      e.preventDefault();
+      if (this.newComment) {
+        axios
+          .post(
+            `${process.env.VUE_APP_URI}blog/comments/asthriona.space/${this.$route.params.slug}`,
+            {
+              content: this.newComment.content,
+            },
+            { headers: { Authorization: localStorage.getItem("token") } }
+          )
+          .then(() => {
+            this.newCommentSent = true;
+            this.loading.comment = false;
+            axios
+              .get(
+                `${process.env.VUE_APP_URI}blog/comments/asthriona.space/${this.$route.params.slug}`
+              )
+              .then((res) => {
+                this.comments = res.data.comments;
+                this.loading.comment = true;
+              });
+          });
+      }
     },
-    methods: {
-        submitComment(e) {
-            e.preventDefault();
-            if (this.newComment) {
-                axios.post(`${process.env.VUE_APP_URI}blog/comments/asthriona.space/${this.$route.params.slug}`, {
-                    content: this.newComment
-                }, { headers: { 'Authorization': localStorage.getItem('token')}}).then((res) => {
-                    this.comments.push(res.data.comment);
-                    this.comment = "";
-                });
-            }
-        }
-    }
+  },
 };
-
 </script>
 
 <style>
-.thumbnail{
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat !important;
-    height: 300px !important;
-    width: 100% !important;
+.thumbnail {
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat !important;
+  height: 300px !important;
+  width: 100% !important;
 }
 .content pre {
   color: #aa3f81 !important;
@@ -150,7 +210,7 @@ export default {
   color: #aa3f81 !important;
 }
 .content code {
-    color: #aa3f81 !important;
+  color: #aa3f81 !important;
 }
 .content img {
   max-width: 100%;
