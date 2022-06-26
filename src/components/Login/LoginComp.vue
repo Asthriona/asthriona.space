4d10bd0b9ff9d90a886141f9c8c81f81a6a13518
Makoto Kobayashi <im@asthriona.com>
2022-06-27 03:07:09 +0900

    fix(logins) Add error handling

--- src/components/Login/LoginComp.vue
+++ src/components/Login/LoginComp.vue
@@ -1,55 +1,67 @@
 <template>
-    <div class="loginComp">
-        <v-container>
-            <v-row>
-                <v-form>
-                    <v-text-field
-                        label="Email"
-                        v-model="email"
-                        :rules="[v => !!v || 'Email is required']"
-                    ></v-text-field>
-                    <v-text-field
-                        label="Password"
-                        v-model="password"
-                        :rules="[v => !!v || 'Password is required']"
-                        type="password"
-                    ></v-text-field>
-                    <v-btn
-                        color="primary"
-                        @click="login"
-                        :disabled="!email || !password"
-                    >
-                        Login
-                    </v-btn>
-                </v-form>
-            </v-row>
-        </v-container>
-    </div>
+  <div class="loginComp">
+    <v-container>
+      <v-row>
+        <v-col cols="5" v-if="error.type">
+            <v-alert :type="error.type">
+                <p>{{ error.message }}</p>
+            </v-alert>
+            </v-col>
+            <v-col cols="12" v-if="error.type"></v-col>
+        <v-col cols="5">
+            <v-form>
+          <v-text-field
+            label="Email"
+            v-model="email"
+            :rules="[(v) => !!v || 'Email is required']"
+          ></v-text-field>
+          <v-text-field
+            label="Password"
+            v-model="password"
+            :rules="[(v) => !!v || 'Password is required']"
+            type="password"
+          ></v-text-field>
+          <v-btn color="primary" @click="login" :disabled="!email || !password">
+            Login
+          </v-btn>
+        </v-form>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-    name: "LoginComp",
-    data() {
-        return {
-            email: "",
-            password: "",
-        };
+  name: "LoginComp",
+  data() {
+    return {
+      email: "",
+      password: "",
+      error: {
+        message: "",
+        type: "",
+      },
+    };
+  },
+  methods: {
+    login(event) {
+      event.preventDefault();
+      axios
+        .post(`${process.env.VUE_APP_URI}login/login`, {
+          email: this.email,
+          password: this.password,
+        })
+        .then((res) => {
+          localStorage.setItem("token", res.data.token);
+          return (window.location.href = "/");
+        })
+        .catch((err) => {
+          this.error.type = "error";
+          this.error.message = err.response.data.message;
+        });
     },
-    methods: {
-        login(event) {
-            event.preventDefault();
-            axios.post(`${process.env.VUE_APP_URI}login/login`, {
-                email: this.email,
-                password: this.password,
-            }).then((res) => {
-                localStorage.setItem('token', res.data.token);
-                return window.location.href = "/";
-            }).catch((err) => {
-                console.log(err);
-            });
-        },
-    },
+  },
 };
 </script>
