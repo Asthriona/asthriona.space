b5cac0118d0261dda471db1b2f0354756e130d37
Makoto Kobayashi <im@asthriona.com>
2022-06-27 03:07:45 +0900

    fix(logins): Add redirect if already loged in

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -35,5 +35,10 @@ export default {
         RegisterComp,
         LoginComp,
     },
+    beforeCreate() {
+        if (localStorage.getItem('token')) {
+            window.location.href = '/';
+        }
+    }
 }
 </script>
