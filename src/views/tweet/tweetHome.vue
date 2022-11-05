7c4b178d6097012eab27bd565bc656d2047f4b40
Makoto Kobayashi <im@asthriona.com>
2022-11-05 09:04:49 +0100

    feat(Template Logic): :sparkles: Add Tweethome

--- src/views/tweet/tweetHome.vue
+++ src/views/tweet/tweetHome.vue
@@ -1,38 +1,36 @@
 <template>
     <div class="tweetHome">
-        <div class="tweet" v-for="tweet in tweets" :key="tweet.id">
-        <div class="author">
-            <v-avatar>
-                <v-img :src="tweet.author.avatar"></v-img>
-            </v-avatar>
-            <div class="author-name">
-                <span>{{ tweet.author.displayName }}</span>#
-                <span>{{ tweet.author.discriminator }}</span> <br>
-                <small>@{{tweet.author.username}}</small>
-
-            </div>
-            <div class="tweetContent">
-                <span>{{ tweet.content }}</span>
-            </div>
-            <div class="tweetInfo">
-                <span>{{ tweet.createdAt }}</span>
-                <!-- <span>{{ tweet.likes.length || 0 }}</span>
-                <span>{{ tweet.retweets.length || 0 }}</span> -->
-            </div>
-        </div>
-        </div>
+        <v-container>
+            <v-row>
+                <v-col cols="12" sm="12" md="12" lg="12">
+                    <send-tweet :user="user" />
+                </v-col>
+                <v-col cols="12" sm="12" md="12" lg="12">
+                    <p class="text-center muted">Updated: {{ updatedAt }}</p>
+                    <div class="display-1 text-center">Latest Posts</div>
+                    <timeline :tweets="tweets" :user="user" />
+                </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
 
 <script>
 import axios from 'axios';
+import sendTweet from '@/components/tweet/sendTweet.vue';
+import timeline from '@/components/tweet/timeline.vue';
 export default {
     name: "tweetHome",
     props: ["user"],
     data() {
         return {
             tweets: [],
+            updatedAt: null
         };
+    },
+    components: {
+        sendTweet,
+        timeline,
     },
     beforeMount() {
         this.getTweets();
@@ -49,6 +47,7 @@ export default {
                 .get(`${process.env.VUE_APP_URI}tweet/latest`)
                 .then((res) => {
                     this.tweets = res.data.tweets;
+                    this.updatedAt = new Date().toLocaleString();
                 })
                 .catch((err) => {
                     console.log(err);
@@ -57,3 +56,10 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.muted {
+    color: #aaa;
+    font-size: 0.8rem;
+}
+</style>
