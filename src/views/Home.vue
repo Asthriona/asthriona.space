ff80830577c1fc6abe24553269ffbd759dead089
Makoto Kobayashi <im@asthriona.com>
2023-02-20 06:27:07 +0100

    Update and make better random lines

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,128 +1,103 @@
-<template>
-  <!-- 
+<template><!-- 
   Make this page dynamic with projects and everything can be changed at anythime via the gateway.
  -->
-  <div class="home">
-    <div class="head-banner">
-      <div class="header-content">
-        <v-col cols="12" class="avatar d-flex justify-center">
-          <v-avatar size="250" class="mt-4" color="black">
-            <v-img
-            style="opacity: 0.3;"
-              src="https://pbs.twimg.com/profile_images/1523324400245825537/zQiBhAZV_400x400.jpg"
-            ></v-img>
-          </v-avatar>
-        </v-col>
-        <span>
-          <div class="display-4 text-center font-logo">Asthriona</div>
-        </span>
-        <span>
-          <div class="display-2 text-center randomText">{{ chosenLine }}</div>
-        </span>
-      </div>
-      <div class="subline">
-        <v-col cols="12" class="d-flex justify-center"> </v-col>
-      </div>
-    </div>
-    <hr />
-    <v-spacer></v-spacer>
-    <v-container>
-      <v-row align="center" justify="center">
-        <v-col cols="6" class="m">
-          <div class="base mt-4 text-center display-1">
-            Welcome to my shitpost place!
-          </div>
-          <div class="Infos mt-4 text-center">
-            <span>
-              <v-icon>mdi-information</v-icon> Information about this space...
-              <span>
-                <p>
-                  This space is random... I will post pretty much anything that
-                  goes through my head, nothing really serious (I mean about
-                  work). if you are a recruiter or someone who want to work with
-                  me, Please visite my
-                  <a href="asthriona.com">more serious website</a>. also you
-                  should know that both website has been made by myself, from
-                  the ground up.<br />
+   <div class="home">
+     <div class="head-banner">
+       <div class="header-content">
+         <v-col cols="12" class="avatar d-flex justify-center">
+           <v-avatar size="250" class="mt-4" color="black">
+             <v-img style="opacity: 0.3;"
+               src="https://pbs.twimg.com/profile_images/1523324400245825537/zQiBhAZV_400x400.jpg"></v-img>
+           </v-avatar>
+         </v-col>
+         <span>
+           <div class="display-4 text-center font-logo">Asthriona</div>
+         </span>
+         <span>
+           <div class="display-2 text-center randomText">{{ chosenLine }}</div>
+         </span>
+       </div>
+       <div class="subline">
+         <v-col cols="12" class="d-flex justify-center"> </v-col>
+       </div>
+     </div>
+     <hr />
+     <v-spacer></v-spacer>
+     <v-container>
+       <v-row align="center" justify="center">
+         <v-col cols="6" class="m">
+           <div class="base mt-4 text-center display-1">
+             Welcome to my shitpost place!
+           </div>
+           <div class="Infos mt-4 text-center">
+             <span>
+               <v-icon>mdi-information</v-icon> Information about this space...
+               <span>
+                 <p>
+                   This space is random... I will post pretty much anything that
+                   goes through my head, nothing really serious (I mean about
+                   work). if you are a recruiter or someone who want to work with
+                   me, Please visite my
+                   <a href="asthriona.com">more serious website</a>. also you
+                   should know that both website has been made by myself, from
+                   the ground up.<br />
 
-                  else, welcome! :D Let's be weird together! <br>
-                </p>
-              </span>
-            </span>
-          </div>
-        </v-col>
-        <v-col cols="12">
-          <h1 class="heading">My Works</h1>
-          I've been last seen working on <a :href="gitHub.repo.url" target="_blank" rel="noopener noreferrer">{{ gitHub.repo.name || "Network Error..." }}</a> <code>{{ gitHub.commits.push_id || "null" }}</code>
-        </v-col>
-        <v-col 
-        v-for="work in works" 
-        :key="work.title"
-        xs="12"
-        sm="6"
-        md="6"
-        lg="3"
-        xl="2">
-          <!-- Works -->
-              <v-card >
-                <v-img :src="work.img" lazy></v-img>
-                <v-card-title>{{ work.title }}</v-card-title>
-                <v-card-text>{{ work.description }}</v-card-text>
-                <v-card-actions>
-                  <v-btn
-                    :href="work.url"
-                    target="_blank"
-                    color="primary"
-                    dark
-                    :disabled="!work.url">
-                    <v-icon>mdi-link</v-icon>
-                    Visit
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-        </v-col>
-        <v-col cols="12">
-          <h1>Anime</h1>
-          In order of preferance. <br />
-          Last activity: <code><span class="lastAnime"></span></code> <br />
-          Anilist Profile: <a href="https://anilist.co/user/Asthriona/" target="_blank" rel="noopener noreferrer">Asthriona</a>
-          </v-col>
-          <v-col
-          v-for="anime in animes"
-              :key="anime.title"
-          xs="12"
-        sm="6"
-        md="6"
-        lg="3"
-        xl="2">
-            <v-card>
-              <v-img :src="anime.img" max-height="450" lazy></v-img>
-              <v-card-title>{{ anime.title }}</v-card-title>
-              <v-card-text>{{ anime.description }}</v-card-text>
-              <v-card-actions>
-                <v-btn
-                  :href="anime.url"
-                  target="_blank"
-                  color="primary"
-                  dark
-                  :disabled="!anime.url">
-                  <v-icon>mdi-link</v-icon>
-                  Anilist
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-          <v-col cols="12" class="mt-4">
-            <h1 class="text-center display-1 mt-4">Asthriona proudly sponsors:</h1>
-            <!-- <p class="muted text-center mb-4">sponsoring for 30 000 yen</p> -->
-            <v-col cols="12" class="d-flex justify-space-around mb-6 mt-6 sponsor">
-              <img src="https://cdn.asthriona.com/static/good-smile-racing.png" height="120px" />
-              <img src="https://cdn.asthriona.com/static/personal-sponsor-2022.png" height="120px" />
-            </v-col>
-          </v-col>
-      </v-row>
-    </v-container>
-  </div>
+                   else, welcome! :D Let's be weird together! <br>
+                 </p>
+               </span>
+             </span>
+           </div>
+         </v-col>
+         <v-col cols="12">
+           <h1 class="heading">My Works</h1>
+           I've been last seen working on <a :href="gitHub.repo.url" target="_blank" rel="noopener noreferrer">{{
+             gitHub.repo.name || "Network Error..." }}</a> <code>{{ gitHub.commits.push_id || "null" }}</code>
+         </v-col>
+         <v-col v-for="work in works" :key="work.title" xs="12" sm="6" md="6" lg="3" xl="3">
+           <!-- Works -->
+           <v-card>
+             <v-img :src="work.img" lazy></v-img>
+             <v-card-title>{{ work.title }}</v-card-title>
+             <v-card-text>{{ work.description }}</v-card-text>
+             <v-card-actions>
+               <v-btn :href="work.url" target="_blank" color="primary" dark :disabled="!work.url">
+                 <v-icon>mdi-link</v-icon>
+                 Visit
+               </v-btn>
+             </v-card-actions>
+           </v-card>
+         </v-col>
+         <v-col cols="12">
+           <h1>Anime</h1>
+           In order of preferance. <br />
+           Last activity: <code><span class="lastAnime"></span></code> <br />
+           Anilist Profile: <a href="https://anilist.co/user/Asthriona/" target="_blank"
+             rel="noopener noreferrer">Asthriona</a>
+         </v-col>
+         <v-col v-for="anime in animes" :key="anime.title" xs="12" sm="6" md="6" lg="3" xl="3">
+           <v-card>
+             <v-img :src="anime.img" max-height="450" lazy></v-img>
+             <v-card-title>{{ anime.title }}</v-card-title>
+             <v-card-text>{{ anime.description }}</v-card-text>
+             <v-card-actions>
+               <v-btn :href="anime.url" target="_blank" color="primary" dark :disabled="!anime.url">
+                 <v-icon>mdi-link</v-icon>
+                 Anilist
+               </v-btn>
+             </v-card-actions>
+           </v-card>
+         </v-col>
+         <v-col cols="12" class="mt-4">
+           <h1 class="text-center display-1 mt-4">Asthriona proudly sponsors:</h1>
+           <!-- <p class="muted text-center mb-4">sponsoring for 30 000 yen</p> -->
+           <v-col cols="12" class="d-flex justify-space-around mb-6 mt-6 sponsor">
+             <img src="https://cdn.asthriona.com/static/good-smile-racing.png" height="120px" />
+             <img src="https://cdn.asthriona.com/static/personal-sponsor-2022.png" height="120px" />
+           </v-col>
+         </v-col>
+       </v-row>
+     </v-container>
+   </div>
 </template>
 
 <script>
@@ -152,19 +127,19 @@ export default {
           url: "https://yukiko.app",
           img: "https://cdn.asthriona.com/i/2022/05/Yukiko%20-%20Discord%20Bot.png"
         },
-        {
-          title: "Rainboom Studio",
-          description: "Game studio.",
-          url: "https://rainboomstudio.com/",
-          img: "https://cdn.asthriona.com/i/2022/05/rainboomStudio.png"
-        },
+        // {
+        //   title: "Rainboom Studio",
+        //   description: "Game studio.",
+        //   url: "https://rainboomstudio.com/",
+        //   img: "https://cdn.asthriona.com/i/2022/05/rainboomStudio.png"
+        // },
         {
           title: "CablePorn",
           description: "Automated Twitter Account.",
           url: "https://twitter.com/CablePorn_",
           img: "https://cdn.asthriona.com/i/2022/05/cablePorn.png"
         },
-        
+
       ],
       animes: [
         {
@@ -196,19 +171,34 @@ export default {
           description: "Uesugi Fuutarou, a high school second-year from a poor family, receives a highly appealing offer to work part-time as a tutor.",
           url: "https://anilist.co/anime/103572/The-Quintessential-Quintuplets/",
           img: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx103572-2MXV30htoCSh.png"
-        }
+        },
+        {
+          title: "Durarara!!",
+          description: "In Tokyo's downtown district of Ikebukuro, amidst many strange rumors and warnings of the existence of a headless \"Black Rider\"",
+          url: "https://anilist.co/anime/6746/Durarara/",
+          img: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx6746-Q4EmstN2fy0R.png"
+        },
+        {
+          title: "Boogiepop and Others",
+          description: "There is an urban legend that children tell one another about a shinigami that can release people from the pain they may be suffering.",
+          url: "https://anilist.co/anime/101283/Boogiepop-and-Others/",
+          img: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx101283-YpZqD7wrzWui.jpg"
+        },
+        {
+          title: "Wangan Midnight",
+          description: "The story gets its roots from the actual street racing that occurs on Tokyo's Shuto Expressway, one stretch of which is known as the \"Wangan\"",
+          url: "https://anilist.co/anime/2608/Wangan-Midnight/",
+          img: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx2608-J8zVMG1LO7CS.jpg"
+        },
       ],
       lastAnime: {},
       rdmText: [
         "/*This line is disabled*/",
-        "死にたい",
-        "空に消えたい",
-        "Better keep it “just me”, It’s better that way, trust me",
-        "it’s Me, Myself and I again",
         "Hello from Speedtest.net!",
-        "Welcome to 2020 Season 3!",
-        "You can now play 2021 as luigi",
-        "Click here to cancel your subscription to 2022",
+        `Welcome to 2020 Season ${new Date().getFullYear() + 1 - 2020}!`,
+        `You can now play ${new Date().getFullYear() - 1} as luigi`,
+        `Click here to cancel your subscription to ${new Date().getFullYear()}`,
+        "Every brilliant day should be lived for those who passed away."
       ],
       chosenLine: "",
       gitHub: {
@@ -217,8 +207,10 @@ export default {
       },
     };
   },
-    mounted() {
-      axios.post('https://graphql.anilist.co', {
+  mounted() {
+    // get current year
+    this.chosenLine = this.rdmText[Math.floor(Math.random() * this.rdmText.length)];
+    axios.post('https://graphql.anilist.co', {
       query: `
       query{
         Page(perPage: 10) {
@@ -245,32 +237,32 @@ export default {
         id: 101386
       }
     })
-    .then((res) => {
-      const anime = res.data.data.Page.activities[0];
-      const status = res.data.data.Page.activities[0].status;
-      const lastText = `${status.charAt(0).toUpperCase() + status.slice(1)} ${anime.media.title.english} (${anime.progress == null ? anime.media.episodes : anime.progress.split("-")[1].replace(/\s/g, '') }/${anime.media.episodes})`;
-      const lastUrl = anime.media.siteUrl;
-      // get element with class "lastAnime"
-      const lastAnime = document.querySelector(".lastAnime");
-      // add a:blank to the element
-      const a = document.createElement("a");
-      a.href = lastUrl;
-      a.innerText = lastText;
-      lastAnime.appendChild(a);
-      return this.lastAnime;
-    })
-    .catch((err) => {
-      console.error(err);
-      const lastAnime = document.querySelector(".lastAnime");
-      // set text to the element
-      lastAnime.innerText = `Yikes, An error happened :c ${err.response.status} ${err.response.statusText}`;
-    })
-    },
-    methods: {
-      anilist() {
-        
-      }
-    },
+      .then((res) => {
+        const anime = res.data.data.Page.activities[0];
+        const status = res.data.data.Page.activities[0].status;
+        const lastText = `${status.charAt(0).toUpperCase() + status.slice(1)} ${anime.media.title.english} (${anime.progress == null ? anime.media.episodes : anime.progress.split("-")[1].replace(/\s/g, '')}/${anime.media.episodes})`;
+        const lastUrl = anime.media.siteUrl;
+        // get element with class "lastAnime"
+        const lastAnime = document.querySelector(".lastAnime");
+        // add a:blank to the element
+        const a = document.createElement("a");
+        a.href = lastUrl;
+        a.innerText = lastText;
+        lastAnime.appendChild(a);
+        return this.lastAnime;
+      })
+      .catch((err) => {
+        console.error(err);
+        const lastAnime = document.querySelector(".lastAnime");
+        // set text to the element
+        lastAnime.innerText = `Yikes, An error happened :c ${err.response.status} ${err.response.statusText}`;
+      })
+  },
+  methods: {
+    anilist() {
+
+    }
+  },
   // created() {
   //   // add twitter card to head
   //   document.head.innerHTML += `<meta name="twitter:card" content="summary" />`;
@@ -296,6 +288,7 @@ export default {
   font-family: Asthriona;
   src: url(../assets/font/logofont.ttf) format("truetype");
 }
+
 .head-banner {
   background-image: url(https://cdn.asthriona.com/i/2022/05/nihon-graphy-eeUmYVTkFg4-unsplash.jpg);
   background-size: cover;
@@ -306,18 +299,20 @@ export default {
   -webkit-text-stroke-width: 3px;
   -webkit-text-stroke-color: black;
 }
+
 .font-logo {
   font-family: Asthriona !important;
   color: #000;
   -webkit-text-stroke: 3px #fff;
 }
+
 .randomText {
   -webkit-text-stroke-width: 1px;
   -webkit-text-stroke-color: black;
 }
+
 .muted {
   color: #999;
   font-size: 0.8em;
 }
-
 </style>
